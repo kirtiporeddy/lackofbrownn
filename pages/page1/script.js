673f7d9e653c7756e637457fb144e6861a1971c9
kirtiporeddy <porek424@newschool.edu>
2023-03-30 23:49:02 -0400

    Initial commit

---
+++ pages/page1/script.js
@@ -0,0 +1,275 @@
+const albums = 
+[
+    {
+      "Brand": "Ponds",
+      "Name": "Pond's White Beauty Spot-Less Fairness Cream",
+      "Year_of_Release": 2014,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/pondsone.png"
+    },
+    {
+      "Brand": "Fair and lovely",
+      "Name": "Fair & Lovely Advanced Multi Vitamin Cream",
+      "Year_of_Release": 1975,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/fairandlovely.png"
+    },
+    {
+      "Brand": "Olay",
+      "Name": "Olay Natural White All-In-One Fairness Cream",
+      "Year_of_Release": 2009,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/olayone.png"
+    },
+    {
+      "Brand": "Nivea",
+      "Name": "Nivea Visage Sparkling Glow Fairness Cream",
+      "Year_of_Release": 2011,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/nivea.png" },
+    {
+      "Brand": "Garnier",
+      "Name": "Garnier Skin Naturals Light Complete Fairness Cream",
+      "Year_of_Release": 2012,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/garnier.png"},
+    {
+      "Brand": "Ponds",
+      "Name": "Ponds Flawless White Dewy Rose Whitening Soft Gel",
+      "Year_of_Release": 2008,
+      "Country_of_Release": "Thailand",
+      "Product_Type": "Gel Cream",
+      "Photo": "../images/pondstwo.png"
+    },
+    {
+      "Brand": "Vaseline",
+      "Name": "Vaseline Healthy White Lightening Lotion",
+      "Year_of_Release": 2008,
+      "Country_of_Release": "Philippines",
+      "Product_Type": "Lotion",
+      "Photo": "../images/vaseline.png"
+    },
+    {
+      "Brand": "Ivana Skin",
+      "Name": "Bleaching Cloud Scrub",
+      "Year_of_Release": 2022,
+      "Country_of_Release": "Philippines",
+      "Product_Type": "Scrub",
+      "Photo": "../images/ivanaskin.png" },
+    {
+      "Brand": "Clean & Clear",
+      "Name": "Clean & Clear Fairness Cream",
+      "Year_of_Release": 2013,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/cleanandclear.png"},
+    {
+      "Brand": "Lakmé",
+      "Name": "Lakmé Perfect Radiance Fairness Day Cream",
+      "Year_of_Release": 2011,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/lakme.png"
+    },
+    {
+      "Brand": "Avon",
+      "Name": "Avon Naturals Fairness Cream",
+      "Year_of_Release": 2009,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/avon.png" },
+    {
+      "Brand": "Bio-essence",
+      "Name": "Bio-essence Tanaka White Double Whitening Cream",
+      "Year_of_Release": 2012,
+      "Country_of_Release": "Singapore",
+      "Product_Type": "Cream",
+      "Photo": "../images/bio.png"
+    },
+    {
+      "Brand": "Natura Bisse",
+      "Name": "Natura Bissé Diamond White Rich Luxury Cleanse",
+      "Year_of_Release": 2011,
+      "Country_of_Release": "Spain",
+      "Product_Type": "Cleanser",
+      "Photo": "../images/natura.png" },
+    {
+      "Brand": "Kose Sekkisei",
+      "Name": "Kose Sekkisei White BB Cream",
+      "Year_of_Release": 2012,
+      "Country_of_Release": "Japan",
+      "Product_Type": "Cream",
+      "Photo": "../images/sek.png" },
+    {
+      "Brand": "Estée Lauder",
+      "Name": "Estée Lauder Crescent White Full Cycle Brightening Moisture Treatment",
+      "Year_of_Release": 2015,
+      "Country_of_Release": "United States",
+      "Product_Type": "Moisturiser",
+      "Photo": "../images/estee.png"
+    },
+    {
+      "Brand": "Chanel",
+      "Name": "Chanel Le Blanc Whitening Moisturizing Cream",
+      "Year_of_Release": 2015,
+      "Country_of_Release": "France",
+      "Product_Type": "Cream",
+      "Photo": "../images/chanel.png" },
+    {
+      "Brand": "La Mer",
+      "Name": "La Mer The Brilliance White Infusion",
+      "Year_of_Release": 2015,
+      "Country_of_Release": "United States",
+      "Product_Type": "Infusion Cream",
+      "Photo": "../images/lamer.png" },
+    {
+      "Brand": "Shiseido",
+      "Name": "Shiseido White Lucent Brightening Moisturizing Cream",
+      "Year_of_Release": 2010,
+      "Country_of_Release": "Japan",
+      "Product_Type": "Cream",
+      "Photo": "../images/shi.png"
+    },
+    {
+      "Brand": "Olay",
+      "Name": "Olay White Radiance Light Perfecting Day Cream SPF24",
+      "Year_of_Release": 2012,
+      "Country_of_Release": "Philippines",
+      "Product_Type": "Cream",
+      "Photo": "../images/olaytwo.png" },
+    {
+      "Brand": "Tony Mony",
+      "Name": "Panda's Dream White Magic Cream",
+      "Year_of_Release": 2014,
+      "Country_of_Release": "South Korea",
+      "Product_Type": "Cream",
+      "Photo": "../images/tony.png"
+    },
+    {
+      "Brand": "Sulwhasoo",
+      "Name": "Snowise Brightening Cream",
+      "Year_of_Release": 2013,
+      "Country_of_Release": "South Korea",
+      "Product_Type": "Cream",
+      "Photo": "../images/sul.png" },
+    {
+      "Brand": "L'Oreal",
+      "Name": "L'Oreal Paris White Perfect Fairness Cream",
+      "Year_of_Release": 2009,
+      "Country_of_Release": "India",
+      "Product_Type": "Cream",
+      "Photo": "../images/loreal.png"
+    },
+    {
+      "Brand": "Whitenious",
+      "Name": "Whitenious fairness cream",
+      "Year_of_Release": 2014,
+      "Country_of_Release": "Nigeria",
+      "Product_Type": "Cream",
+      "Photo": "../images/white.png"
+    },
+    {
+      "Brand": "Olay",
+      "Name": "White Radiance Light Perfecting Essence",
+      "Year_of_Release": 2006,
+      "Country_of_Release": "Philippines",
+      "Product_Type": "Serum",
+      "Photo": "../images/olay3.png"
+    },
+    {
+      "Brand": "Lancôme",
+      "Name": "Lancôme Blanc Expert Melanolyser",
+      "Year_of_Release": 2013,
+      "Country_of_Release": "France",
+      "Product_Type": "Serum",
+      "Photo": "../images/lancome.png" },
+    {
+      "Brand": "Innisfree",
+      "Name": "Whitening Pore Synergy Serum",
+      "Year_of_Release": 2014,
+      "Country_of_Release": "South Korea",
+      "Product_Type": "Serum",
+      "Photo": "../images/innisfree.png"
+    }
+  ]
+  const albumsList = document.getElementById('albums-list')
+  
+  function renderAlbum(album) {
+    // Create .album element
+    const albumElement = document.createElement('div')
+  
+    albumElement.classList.add('album')
+  
+    console.log(albumElement)
+    console.log(album.cover_image_url)
+  
+    // Create the inner HTML content
+    const albumHTML = `
+      <img src="${album.Photo}">
+      <ul>
+        <li>Brand of Product: ${album.Brand}</li>
+        <li>Name of Product: ${album.Name}</li>
+        <li>Year of Release: ${album.Year_of_Release}</li>
+        <li>Type of Product: ${album.Product_Type}</li>
+        <li>Country of Release: ${album.Country_of_Release}</li>
+    
+        <li>
+        <a href="../page2/index.html">
+          BUY ME
+          </a>
+        <li>
+      </ul>
+    `
+
+
+
+var modes = document.body;
+var isDark = false;
+
+var darkModeButton = document.getElementById("buttonDarkMode");
+darkModeButton.addEventListener("click", changeToDarkMode);
+
+function changeToDarkMode() {
+  if(isDark === true) {
+  
+  darkModeButton.textContent = "Dark mode";
+  modes.style.background = "#ffe2c9";
+  modes.style.color = "#2E1603";
+  buttonDarkMode.style.color = "#ffe2c9";
+  buttonDarkMode.style.background = "#2E1603";
+  
+  isDark = false;
+  console.log("dark mode is off.");
+    
+  } else if(isDark === false){
+    
+  darkModeButton.textContent = "Light mode";
+  modes.style.background = "#2E1603";
+  modes.style.color = "#ffe2c9";
+  buttonDarkMode.style.color = "#2E1603";
+  buttonDarkMode.style.background = "#ffe2c9";
+ 
+  isDark = true;
+  console.log("dark mode is on!");
+    
+  }
+}
+  
+    // Set the inner HTML content of the albumElement
+    albumElement.innerHTML = albumHTML
+  
+    // Add to the list #albums-list
+    albumsList.appendChild(albumElement)
+  }
+  
+  // Start App!
+  albums.forEach(function(album) {
+    // if (album.year_released < 2000) {
+      renderAlbum(album)
+    // }
+  })
---
+++ pages/page2/script.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<title>error</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	</head>
+	<body>
+
+		<h1>This product is unethical and may cause rashes, dermatis, darkening of the skin, neurological problems, fetal growth retardation, and possibly cancer. </h1>
+
+	</body>
+	</html>
